9d77ef518a2056dffae5dca8926734e827b6120c
AllMightyyyy <zakariafarih142@gmail.com>
2025-04-01 02:28:23 +0200

    fixed problems

--- zblog-frontend/src/types/tinymce.d.ts
+++ zblog-frontend/src/types/tinymce.d.ts
@@ -1,27 +1,33 @@
 declare module 'tinymce/tinymce' {
-    interface Editor {
-      on(event: string, callback: Function): void;
-      getContent(options: { format: string }): string;
-      setContent(content: string): void;
-    }
+  interface EditorEvent {
+    command?: string;
+    type: string;
+  }
+
+  interface Editor {
+    on(event: 'init', callback: () => void): void;
+    on(event: 'ExecCommand', callback: (e: EditorEvent) => void): void;
+    getContent(options: { format: string }): string;
+    setContent(content: string): void;
+  }
+}
+
+declare module 'tinymce/*' {
+  const content: any;
+  export = content;
+}
+
+declare module '@tinymce/tinymce-react' {
+  import { Editor as TinyMCEEditor } from 'tinymce/tinymce';
+  
+  interface IAllProps {
+    value?: string;
+    onEditorChange?: (value: string, editor: TinyMCEEditor) => void;
+    init?: {
+      setup?: (editor: TinyMCEEditor) => void;
+      [key: string]: any;
+    };
   }
   
-  declare module 'tinymce/*' {
-    const content: any;
-    export = content;
-  }
-  
-  declare module '@tinymce/tinymce-react' {
-    import { Editor as TinyMCEEditor } from 'tinymce/tinymce';
-    
-    interface IAllProps {
-      value?: string;
-      onEditorChange?: (value: string, editor: TinyMCEEditor) => void;
-      init?: {
-        setup?: (editor: TinyMCEEditor) => void;
-        [key: string]: any;
-      };
-    }
-    
-    export class Editor extends React.Component<IAllProps> {}
-  }
+  export class Editor extends React.Component<IAllProps> {}
+}
